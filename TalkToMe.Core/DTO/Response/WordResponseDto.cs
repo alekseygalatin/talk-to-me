73d2ca2d2b5d1b3927e687eb8850e5d68467b34b
Oleg <shishkin_o@iuca.kg>
2025-01-14 17:53:30 +0600

    transcription added to model

--- TalkToMe.Core/DTO/Response/WordResponseDto.cs
+++ TalkToMe.Core/DTO/Response/WordResponseDto.cs
@@ -5,6 +5,7 @@ namespace TalkToMe.Core.DTO.Response;
 public class WordResponseDto
 {
     public string LanguageWord { get { return $"{Language}#{Word}"; } }
+    public string Transcription { get; set; } = default!;
     public string Language { get; set; } = default!;
     public string Word { get; set; } = default!;
     public List<string> Translations { get; set; } = new List<string>();
