817415bab263d9380a5a7bf39ccd6bdbc5484da8
aleksei.galatin <aleksei.galatin@quokka.se>
2025-02-01 00:35:26 +0100

    SessionId for history controller

--- TalkToMe/Controllers/HistoryController.cs
+++ TalkToMe/Controllers/HistoryController.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Security.Claims;
 using Amazon.TranscribeService.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,16 +20,17 @@ public class HistoryController : ControllerBase
     {
         _historyService = historyService;
     }
+    
+    private string SessionId => HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
         
     [HttpGet("{locale}/{agent}")]
     public async Task<ActionResult> Invoke([FromRoute] string locale, [FromRoute] string agent)
     {
-        var sub = this.HttpContext.User.Claims.First(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")).Value;
         if (agent == "conversationAgent")
         {
             if (locale.Equals("sv-se", StringComparison.OrdinalIgnoreCase))
             {
-                var history = await _historyService.GetHistory(sub + "6a47a518-cc4b-4b40-b7a5-94113d8c1b16");
+                var history = await _historyService.GetHistory(SessionId + "6a47a518-cc4b-4b40-b7a5-94113d8c1b16");
                 var result = history.Select(x => new HistoryMessageDto
                 {
                     Message = x.Message,
@@ -39,7 +41,7 @@ public class HistoryController : ControllerBase
             }
             else
             {
-                var history = await _historyService.GetHistory(sub + "7b542e6e-7e2a-424f-8e76-0fec63cb2568");
+                var history = await _historyService.GetHistory(SessionId + "7b542e6e-7e2a-424f-8e76-0fec63cb2568");
                 var result = history.Select(x => new HistoryMessageDto
                 {
                     Message = x.Message,
@@ -53,7 +55,7 @@ public class HistoryController : ControllerBase
         {
             if (locale.Equals("sv-se", StringComparison.OrdinalIgnoreCase))
             {
-                var history = await _historyService.GetHistory(sub + "f9b126e3-4340-4009-8b72-29a4ec321e7c");
+                var history = await _historyService.GetHistory(SessionId + "f9b126e3-4340-4009-8b72-29a4ec321e7c");
                 var result = history.Select(x => new HistoryMessageDto
                 {
                     Message = x.Message,
@@ -64,7 +66,7 @@ public class HistoryController : ControllerBase
             }
             else
             {
-                var history = await _historyService.GetHistory(sub + "4e4f7fdd-ad93-4189-92af-d711c92aa751");
+                var history = await _historyService.GetHistory(SessionId + "4e4f7fdd-ad93-4189-92af-d711c92aa751");
                 var result = history.Select(x => new HistoryMessageDto
                 {
                     Message = x.Message,
