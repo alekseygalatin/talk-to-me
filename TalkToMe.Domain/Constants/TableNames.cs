69a04f031e94cc510a78dd2b9731768e02e26bfd
Oleg <shishkin_o@iuca.kg>
2025-03-21 20:01:40 +0600

    table names returned

--- TalkToMe.Domain/Constants/TableNames.cs
+++ TalkToMe.Domain/Constants/TableNames.cs
@@ -2,8 +2,8 @@
 {
     public static class TableNames
     {
-        public const string UserPreferences = "UserPreferencesTable";
-        public const string Languages = "LanguagesTable";
+        public const string UserPreferences = "UserPreferences";
+        public const string Languages = "Languages";
         public const string WordsTable = "WordsTable";
         public const string ChatHistory = "ChatHistoryTable";
         public const string QueryCountersTable = "QueryCountersTable";
