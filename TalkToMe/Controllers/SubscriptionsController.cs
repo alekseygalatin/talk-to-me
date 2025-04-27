45aa69f52b9e22749b2b252a781ced3b23594f39
Oleg <shishkin_o@iuca.kg>
2025-04-27 15:52:09 +0600

    subscription functionality implemented

---
+++ TalkToMe/Controllers/SubscriptionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TalkToMe.Core.DTO.Request;
+using TalkToMe.Core.Interfaces;
+using TalkToMe.Helpers;
+
+namespace TalkToMe.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscriptionsController : ControllerBase
+    {
+        private readonly ISubscriptionService _subscriptionService;
+
+        public SubscriptionsController(ISubscriptionService subscriptionService)
+        {
+            _subscriptionService = subscriptionService;
+        }
+
+        [HttpGet]
+        public async Task<bool> SubscriptionRequested()
+        {
+            return await _subscriptionService.SubscriptionRequested(UserHelper.GetUserId(User));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Subscription([FromBody] SubscriptionRequestDto subscription)
+        {
+            subscription.UserId = UserHelper.GetUserId(User);
+
+            await _subscriptionService.RequestSubscription(subscription);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task CancelSubscription()
+        {
+            await _subscriptionService.CancelSubscriptionRequest(UserHelper.GetUserId(User));
+        }
+    }
+}
