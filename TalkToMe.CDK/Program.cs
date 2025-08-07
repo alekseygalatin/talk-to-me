c8fb28a9c9b840d872b18c729d275792879cb25d
aleksei.galatin <aleksei.galatin@quokka.se>
2025-08-07 22:31:12 +0200

    added cdk for agents

---
+++ TalkToMe.CDK/Program.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.CDK;
+
+namespace TalkToMe.CDK;
+
+sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var app = new App();
+            
+        new AgentsStack(app, "AgentsStack", new StackProps
+        {
+            Env = new Amazon.CDK.Environment
+            {
+                Account = "038462779690",
+                Region = "us-east-1",
+            }
+        });
+            
+        app.Synth();
+    }
+}
